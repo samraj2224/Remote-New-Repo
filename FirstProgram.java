e123152c57ebecb938966cbdd2e273a735ff4635
samadhan <samraj2224@gmail.com>
2019-12-01 00:58:22 +0530

    firstprogram added

---
+++ FirstProgram.java
@@ -0,0 +1,17 @@
+class FirstProgram{
+	public static void main(String args[]){
+		System.out.println("Samadhan gapat");
+	samadhan5586();
+	Gapat012345();
+	}
+	public static void samadhan5586(){
+		System.out.println("Sambhaji Kale");
+	}
+    public static void Gapat012345(){
+	System.out.println("Vitthal Zagade");
+	}
+    
+	
+
+}
+
